57801c6fb50250f14601d4d38c1987ddfc518de7
FarhanVirenze <farhanvirenze18@gmail.com>
2023-05-29 11:28:44 +0700

    tambahan fungsi integer main

--- Polimorpi/Polimorpi.cpp
+++ Polimorpi/Polimorpi.cpp
@@ -20,8 +20,15 @@ public:
 };
 
 class lia :public seseorang {
-
+public:
+	void pesan() {
+		cout << "Pesan dari lia" << endl;
+	}
 };
+
+int main() {
+
+}
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
